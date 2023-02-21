d6d0471b1e6613faa72f0ad18e4bc0ef790374f5
Dirk Kramer <46933603+rapidengineering@users.noreply.github.com>
2023-02-21 08:48:46 +0100

    Add project files.

---
+++ HelixMouseWheelZoomTest/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HelixToolkit.SharpDX.Core;
+using HelixToolkit.Wpf.SharpDX;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelixMouseWheelZoomTest
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+           
+
+            // Init camera
+            Camera = SetOrthographicCamera();
+
+
+            // Init model
+            var mesh = new MeshBuilder();
+            mesh.AddBox(new Vector3(0, 0, 0), 2000, 2000, 2000);
+            boxGeometry = mesh.ToMeshGeometry3D();
+            boxMaterial = PhongMaterials.Red;
+
+        }
+
+
+
+        [ObservableProperty]
+        double zoomSpeed;
+
+        [ObservableProperty]
+        Camera camera;
+
+        [ObservableProperty]
+        MeshGeometry3D boxGeometry;
+
+        [ObservableProperty]
+        Material boxMaterial;
+
+        public Camera SetOrthographicCamera()
+        {
+            zoomSpeed = 1;
+            return new OrthographicCamera
+            {
+                Position = new System.Windows.Media.Media3D.Point3D(155, -78, 136),
+                LookDirection = new System.Windows.Media.Media3D.Vector3D(-160, 80, -140),
+                UpDirection = new System.Windows.Media.Media3D.Vector3D(-0.5, 0.4, 0.75),
+                FarPlaneDistance = 20000000,
+                NearPlaneDistance = -20000000,
+                Width = 240000
+            };
+
+        }
+    }
+}
---
+++ HelixMouseWheelZoomTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using HelixToolkit.SharpDX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelixMouseWheelZoomTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        private void view1_MouseMove3D(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void view1_Loaded(object sender, RoutedEventArgs e)
+        {
+            view1.EffectsManager = new DefaultEffectsManager();
+        }
+    }
+}
